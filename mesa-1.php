774fe982bf9c80ac5232bb39abcabf0edb5b592a
Mario Mario <marioramos083@gmail.com>
2021-02-13 12:53:24 -0500

    first

---
+++ mesa-1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mesa 1</title>
+  <link rel="stylesheet" href="utiles/bootstrap.min.css">
+  <script src="utiles/bootstrap.min.js"></script>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="m-2">
+          A
+        </div>
+      </div>
+      <div class="col-md-3"></div>
+      <div class="col-md-3"></div>
+      <div class="col-md-3"></div>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ utiles/base_de_datos.php
@@ -0,0 +1,12 @@
+<?php
+$clave = "melli123";
+$usuario = "pi";
+$nombreBaseDeDatos = "aforo";
+$rutaServidor = "localhost";
+$puerto = "5432";
+try {
+    $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $clave);
+    $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo "Ocurrió un error con la base de datos: " . $e->getMessage();
+}
